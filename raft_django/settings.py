523073acb0de25f670c4733640d0a5b67b848dbf
ngua <rory.hayford@gmail.com>
2020-03-27 15:21:47 +0700

    Reorganized template and static directories: created project-level static/ and templates/ for site-wide use

--- raft_django/settings.py
+++ raft_django/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'raft_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -165,8 +165,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Email settings
 EMAIL_HOST = 'smtp.gmail.com'
